be894b1162712ffb43069690290d796acf21f779
Jon Arney <jarneyg@gmail.com>
2026-06-02 13:02:43 -0700

    Another iteration of the template logic engine.  We should use strategy pattern to encapsulate different object type behavior and add an image type, a download button, and a 'theme' selector.

--- spark/laser-cutters-6090/svg-templates/template.js
+++ spark/laser-cutters-6090/svg-templates/template.js
@@ -20,53 +20,95 @@ function getSubDocument(embedding_element)
 
 var svgDocument = null;
 
+var field_list = []
+
 function templateChanged() {
+
     template_id = document.getElementById("template_id");
     var svgelement = document.getElementById("template");
     template.src = template_id.value;
+    
     console.log("Template value changed");
+
 }
 
 function contentChanged() {
     console.log("Object content changed");
     var svgelement = document.getElementById("template");
     console.log(svgelement)
-    subdoc = getSubDocument(svgelement)
-    console.log(subdoc)
-    svgDocument = subdoc;
+    svgDocument = getSubDocument(svgelement)
+
+    template_fields = svgDocument.querySelectorAll('[*|template-field="true"]');
+
+    inner = "";
+    for (field_element of template_fields) {
+	field_metadata = {
+	    "id": field_element.getAttribute("id"),
+	    "name": field_element.getAttribute("template:name"),
+	    "type": field_element.getAttribute("template:type")
+	};
+	field_list.push(field_metadata);
+
+	inner += "<tr>";
+	inner += "<td>";
+	inner += "<span>" + field_metadata.name + "</span>";
+	inner += "</td>";
+	inner += "<td>";
+	if (field_metadata.type === "text-body") {
+	    inner += "<textarea width=\"100%\" rows=\"10\" id=\"" + field_metadata.id + "\" type=\"text\" onchange=\"valuesChanged()\"></textarea>";
+	}
+	else if (field_metadata.type === "text-field") {
+	    inner += "<input width=\"100%\" id=\"" + field_metadata.id + "\" type=\"text\" onchange=\"valuesChanged()\">";
+	}
+	else {
+	}
+	inner += "</td>";
+	inner += "</tr>";
+
+    }
+    fieldElement = document.getElementById("field-container");
+    fieldElement.innerHTML = inner;
+
+    for (field_element of template_fields) {
+    }
+    
+
 }
 
 function valuesChanged() {
 
     if (svgDocument == null) return;
     
-    title = document.getElementById("title");
-    console.log("New text is " + title.value);
+    for (field_metadata of field_list) {
+	htmlElement = document.getElementById(field_metadata.id);
+	svgElement = svgDocument.getElementById(field_metadata.id);
 
-    text1 = svgDocument.getElementById("text1");
-    text1.textContent = title.value;
+	if (field_metadata.type == "text-field") {
+	    svgElement.textContent = htmlElement.value;
+	}
+	else if (field_metadata.type == "text-body") {
+	    text_list = htmlElement.value.split(/\r?\n/);
+	    tspan_elements = svgElement.getElementsByTagName("tspan");
+	    i = 0;
+	    for (tspan_element of tspan_elements) {
+		if (i < text_list.length) {
+		    tspan_element.textContent = text_list[i];
+		}
+		else {
+		    tspan_element.textContent = "";
+		}
+		i = i + 1;
+	    }
+	}
+
+    }
+
 }
 
 function initialize() {
-    console.log("Initialized Template logic");
-
-    // Regular html 
-    subtitle_element = document.getElementById("subtitle");
-    subtitle_element.textContent = "New Subtitle";
-
-    // TODO: Walk through SVG template
-    // and get template elements.
-    // From those, generate HTML editor elements
-    // and place those into an array.
-    //
-    // OnChange event should
-    // walk through HTML editor elements
-    // array and push values down to template.
-    
-//   text1 = subdoc.getElementById("text1");
-//#    text1.textContent = "New Subtitle";
+    var templateListElement = document.getElementById("template");
+    templateListElement.value = "template_cutting_board_vertical.svg";
+    templateListElement.dispatchEvent(new Event('change'))
+    templateChanged();
 }
-
-
-//text1.textContent = "Custom Title";
 
