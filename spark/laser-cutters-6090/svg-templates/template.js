27df6855392db05e2c78f7473a2cf0b3e38b1743
Jon Arney <jarneyg@gmail.com>
2026-06-02 09:56:19 -0700

    Added laser cutter template prototype

---
+++ spark/laser-cutters-6090/svg-templates/template.js
@@ -0,0 +1,64 @@
+console.log("Starting up, setting listener");
+
+window.addEventListener("load", initialize, false);
+
+function getSubDocument(embedding_element)
+{
+    if (embedding_element.contentDocument) 
+    {
+	return embedding_element.contentDocument;
+    } 
+    else 
+    {
+	var subdoc = null;
+	try {
+	    subdoc = embedding_element.getSVGDocument();
+	} catch(e) {}
+	return subdoc;
+    }
+}
+
+var svgDocument = null;
+
+function changed() {
+
+    if (svgDocument == null) return;
+    
+    title = document.getElementById("title");
+    console.log("New text is " + title.value);
+
+    text1 = svgDocument.getElementById("text1");
+    text1.textContent = title.value;
+}
+
+function initialize() {
+    console.log("Initialized Template logic");
+
+    // Regular html 
+    subtitle_element = document.getElementById("subtitle");
+    subtitle_element.textContent = "New Subtitle";
+
+    // img tag
+    var svgelement = document.getElementById("template");
+    console.log(svgelement)
+    subdoc = getSubDocument(svgelement)
+    console.log(subdoc)
+
+    svgDocument = subdoc;
+
+    // TODO: Walk through SVG template
+    // and get template elements.
+    // From those, generate HTML editor elements
+    // and place those into an array.
+    //
+    // OnChange event should
+    // walk through HTML editor elements
+    // array and push values down to template.
+    
+//   text1 = subdoc.getElementById("text1");
+//#    text1.textContent = "New Subtitle";
+}
+
+
+//text1.textContent = "Custom Title";
+
